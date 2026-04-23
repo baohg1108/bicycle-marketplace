32e69ecd4b1f0a0f4798f2131efb0459246e80e2
nphuowng205 <phuongww111@gmail.com>
2026-04-23 19:00:51 +0700

    feat: add KYC flow, middleware, mail service and update dashboard

--- app/Models/Kyc.php
+++ app/Models/Kyc.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Kyc extends Model
 {
-    //
+    protected $fillable = ['status'];
+    
+    function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\PasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\KycRequestController;
 use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -64,6 +65,14 @@ Route::middleware('auth:admin')
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::put('/profile', [ProfileController::class, 'profileUpdate'])->name('profile.update');
     Route::put('/profile/password', [ProfileController::class, 'passwordUpdate'])->name('profile.password.update');
+
+    // Kyc route
+    Route::get('/kyc-requests', [KycRequestController::class, 'index'])->name('kyc.index');
+    Route::get('/kyc-requests/pending', [KycRequestController::class, 'pending'])->name('kyc.pending');
+    Route::get('/kyc-requests/rejected', [KycRequestController::class, 'rejected'])->name('kyc.rejected');
+    Route::get('/kyc-requests/{kyc_request}', [KycRequestController::class, 'show'])->name('kyc.show');
+    Route::get('/kyc-requests/download/{kyc_request}', [KycRequestController::class, 'download'])->name('kyc.download');
+    Route::post('/kyc-requests/{kyc_request}/update', [KycRequestController::class, 'update'])->name('kyc.update');
 });
 
 Route::get('/admin/dashboard', function () {
