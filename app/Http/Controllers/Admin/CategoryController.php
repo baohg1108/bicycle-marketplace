1de2d72c7ff0d3a093487f1d9dffb7e3a6396bdc
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-05-03 01:01:46 +0700

    feat: build hero section with responsive layout

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Traits\FileUploadTrait;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
@@ -11,132 +11,164 @@ use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
-use Psr\Http\Message\ResponseInterface;
 
 class CategoryController extends Controller implements HasMiddleware
 {
+    use FileUploadTrait;
 
-    static function Middleware() : array
+    public static function Middleware(): array
     {
         return [
-            new Middleware('permission:KYC Management')
+            new Middleware('permission:KYC Management'),
         ];
     }
 
-    function index() : View
+    public function index(): View
     {
         return view('admin.category.index');
     }
-    function store(Request $request) 
+
+    public function store(Request $request)
     {
         $data = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255', 'unique:categories,slug'],
-            'parent_id' => ['nullable', 'exists:categories,id'],
-            'is_active' => ['boolean']
+            'name'        => ['required', 'string', 'max:255'],
+            'slug'        => ['required', 'string', 'max:255', 'unique:categories,slug'],
+            'parent_id'   => ['nullable', 'exists:categories,id'],
+            'is_active'   => ['boolean'],
+            "is_featured" => ['boolean'],
+            "image"       => ["nullable", "image", "max:2048"],
+            "icon"        => ["nullable", "image", "max:1000"],
         ]);
 
-        if($data['parent_id'] ?? null){
+        // prevent circular reference and limit depth to 3
+        if ($data['parent_id'] ?? null) {
             $parent = Category::find($data['parent_id']);
-            $depth = 1;
+            $depth  = 1;
 
-            while ($parent && $parent->parent_id){
+            while ($parent && $parent->parent_id) {
                 $depth++;
                 $parent = $parent->parent;
-                if($depth >=3) break;
+                if ($depth >= 3) {
+                    break;
+                }
+
             }
-            if($depth >= 3){
+            if ($depth >= 3) {
                 throw ValidationException::withMessages([
-                    'parent_id' => 'Maximun depth reached'
+                    'parent_id' => 'Maximun depth reached',
                 ]);
             }
         }
 
         $data['position'] = Category::where('parent_id', $data['parent_id'] ?? null)->max('position') + 1;
 
+        if ($request->hasFile("image")) {
+            $data['image'] = $this->uploadFile($request->file("image"));
+        }
+
+        if ($request->hasFile("icon")) {
+            $data['icon'] = $this->uploadFile($request->file("icon"));
+        }
+
         $category = Category::create($data);
 
         return response()->json(['success' => true, 'message' => 'Category created successfully', 'category' => $category]);
     }
 
-    function update(Request $request, int $id)
+    public function update(Request $request, int $id)
     {
         $category = Category::findOrFail($id);
-        $data = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255', 'unique:categories,slug,' . $category->id],
-            'parent_id' => ['nullable', 'exists:categories,id'],
-            'is_active' => ['boolean']
+        $data     = $request->validate([
+            'name'        => ['required', 'string', 'max:255'],
+            'slug'        => ['required', 'string', 'max:255', 'unique:categories,slug,' . $category->id],
+            'parent_id'   => ['nullable', 'exists:categories,id'],
+            'is_active'   => ['boolean'],
+            "is_featured" => ['boolean'],
+            "image"       => ["nullable", "mimes:jpeg,png,jpg,gif,webp,svg", "max:2048"],
+            "icon"        => ["nullable", "mimes:jpeg,png,jpg,gif,webp,svg", "max:1000"],
         ]);
 
-        if($data['parent_id'] ?? null){
+        if ($data['parent_id'] ?? null) {
             $parent = Category::find($data['parent_id']);
-            $depth = 1;
+            $depth  = 1;
 
-            while ($parent && $parent->parent_id){
+            while ($parent && $parent->parent_id) {
                 $depth++;
                 $parent = $parent->parent;
-                if($depth >=3) break;
+                if ($depth >= 3) {
+                    break;
+                }
+
             }
-            if($depth >= 3){
+            if ($depth >= 3) {
                 throw ValidationException::withMessages([
-                    'parent_id' => 'Maximun depth reached'
+                    'parent_id' => 'Maximun depth reached',
                 ]);
             }
         }
 
-        $data['is_active'] = $data['is_active'] ?? false;
+        $data["is_active"] = $data["is_active"] ?? false;
+
+        if ($request->hasFile("image")) {
+            $data['image'] = $this->uploadFile($request->file("image"));
+        }
+
+        if ($request->hasFile("icon")) {
+            $data['icon'] = $this->uploadFile($request->file("icon"));
+        }
 
         $category->update($data);
 
         return response()->json(['success' => true, 'message' => 'Category updated successfully', 'category' => $category, 'type' => 'update']);
     }
 
-    function updateOrder(Request $request) {
+    public function updateOrder(Request $request)
+    {
         $tree = $request->tree;
-        try{
-        DB::transaction(function() use ($tree){
-            $this->updateTree($tree, null);
-        });
+        try {
+            DB::transaction(function () use ($tree) {
+                $this->updateTree($tree, null);
+            });
 
-        return Response()->json(['success' => true, 'message' =>'Category order updated successfully']);
-        }catch (\throwable $th){
+            return Response()->json(['success' => true, 'message' => 'Category order updated successfully']);
+        } catch (\throwable $th) {
             Log::error('Category Order Update Error:', $th);
             return response()->json(['success' => false, 'message' => $th->getMessage()], 500);
         }
     }
 
-    function updateTree($nodes, $parentId) {
-        foreach($nodes as $position => $node){
+    public function updateTree($nodes, $parentId)
+    {
+        foreach ($nodes as $position => $node) {
             $category = Category::find($node['id']);
             $category->update([
                 'parent_id' => $parentId,
-                'position' => $position
+                'position'  => $position,
             ]);
-            
-            if(isset($node['children']) && is_array($node['children'])) {
+
+            if (isset($node['children']) && is_array($node['children'])) {
                 $this->updateTree($node['children'], $category->id);
             }
         }
     }
 
-    function show(int $id)
+    public function show(int $id)
     {
         $category = Category::findOrFail($id);
         return response()->json($category);
     }
 
-    function destroy(int $id) 
+    public function destroy(int $id)
     {
         $category = Category::findOrFail($id);
-        if($category->children()->count() > 0){
+        if ($category->children()->count() > 0) {
             return response()->json(['error' => true, 'message' => 'Category has children and cannot be deleted'], 422);
         }
         $category->delete();
         return response()->json(['succes' => true, 'message' => 'Category deleted successfully']);
     }
 
-    function getNestedCategories()
+    public function getNestedCategories()
     {
         $categories = Category::getNested();
         return response()->json($categories);
---
+++ docs/process-task.md
@@ -0,0 +1,1 @@
+266: need should have add [products.index] route to category link in hero section 12.52
