b05a92aa2dcc63fd927dd181a5cc2a3499e6ffd8
Giabao11082005 <hoanggiabao110820055@gmail.com>
2026-05-04 11:43:30 +0700

    feat:add variants, price, old price in carts

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -1,10 +1,14 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
     //
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
